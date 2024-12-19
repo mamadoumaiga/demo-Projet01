80241410bee66fd55fd8387f5688d4a84d896350
mamadoumaiga <[yorobefouneke@gmail.com]>
2024-12-19 08:21:06 +0000

    test modifications

---
+++ test.py
@@ -0,0 +1,11 @@
+print("Bonjour")
+print("Aw ni sogoma")
+print(2500)
+tot = 2500
+print(tot)
+
+print("Total = " + str(tot))
+nom = "Mamadou Maiga"
+age = 62
+print("Je m'appelle "+nom+", j'ai "+str(age)+" ans")
+
